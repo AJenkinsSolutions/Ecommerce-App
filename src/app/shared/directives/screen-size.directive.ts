edcae4e486c2dcc25b3df321389ad306a4cff6a1
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 11:30:30 -0500

    Add: Impl Screen Size Directive

--- src/app/shared/directives/screen-size.directive.ts
+++ src/app/shared/directives/screen-size.directive.ts
@@ -1,4 +1,5 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { ScreenSizeSignal } from '../../signals/screen-size-signal';
 
 @Directive({
   selector: '[appScreenSize]',
@@ -6,6 +7,25 @@ import { Directive } from '@angular/core';
 })
 export class ScreenSizeDirective {
 
-  constructor() { }
+
+  updatedScreenSize!: string;
+
+  constructor(private screenSizeSignal: ScreenSizeSignal, private renderer: Renderer2, private el: ElementRef) { }
+
+  private setScreenSize(){
+
+    const width = this.el.nativeElement.ownerDocument.defaultView.innerWidth;
+
+    //1. Check the current screen width 
+    if(width < 600){
+      this.updatedScreenSize = 'small';
+    } else if (width >= 600 && width < 1024) {
+      this.updatedScreenSize = 'medium';
+    } else {
+      this.updatedScreenSize = 'large';
+    }
+    //2. Update the screen size signal
+    this.screenSizeSignal.currentScreenSize.set(this.updatedScreenSize)
+  }
 
 }
