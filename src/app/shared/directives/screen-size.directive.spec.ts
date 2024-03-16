879e1fdc1354ad1e06a83a5ff88617dc3385ddcf
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 11:20:53 -0500

    Add: Screen Size directive

---
+++ src/app/shared/directives/screen-size.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScreenSizeDirective } from './screen-size.directive';
+
+describe('ScreenSizeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScreenSizeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
