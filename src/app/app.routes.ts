7b89901cc93f62f579e6d23d8ea7e795517e84ea
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 15:04:24 -0500

    Feat: Add counter component & routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './pages/counter/counter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'counter',
+        pathMatch: 'full'
+    },
+    {
+        path: 'counter',
+        loadComponent: () => import('./pages/counter/counter.component').then(a => a.CounterComponent)
+    }
+];
