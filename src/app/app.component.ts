3ffbb911c279a7dcf28cf0f487ca05b515a37b09
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 09:57:28 -0500

    Feat: Add Basic Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { HeroComponent } from './core/components/hero/hero.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent,
+    MatSidenavModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
