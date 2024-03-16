6a050a2631533bb3cf8aa90293d863e197fe0e24
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 16:27:26 -0500

    feat: create reducer

---
+++ src/app/ngrx/reducers/counter.reducer.ts
@@ -0,0 +1,25 @@
+import { createReducer, on } from "@ngrx/store";
+import * as CounterActions from "../actions/counter.action"
+
+
+export interface ICounterState {
+    count : number
+}
+
+
+export const initalCounterState: ICounterState = {
+    count : 0
+}
+
+
+export const counterReducer = createReducer(
+    initalCounterState,
+ 
+    on(CounterActions.increment, state => ({...state, count: state.count + 1 })),
+    
+    on(CounterActions.decrement, state => ({...state, count: state.count - 1 })),
+    
+    on(CounterActions.reset, state => ({...state, count: 0 }))
+    
+);
+
