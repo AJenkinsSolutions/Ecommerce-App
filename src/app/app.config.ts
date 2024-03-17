0a5e931e9fa83cd214d408c49c244b6008924c20
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-17 10:16:07 -0500

    feat: provide state & effects to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,9 @@ import { provideState, provideStore } from '@ngrx/store';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { counterReducer } from './ngrx/reducers/counter.reducer';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { productListReducer } from './ngrx/reducers/product-list.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { ProductListEffect } from './ngrx/effects/product-list.effect';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,7 +21,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     provideStore(),
     provideStoreDevtools({maxAge: 25}),
-    provideState('counter', counterReducer)
+    provideState('counter', counterReducer),
+    provideState('productList', productListReducer),
+    provideEffects(ProductListEffect)
     
   ]
 };
