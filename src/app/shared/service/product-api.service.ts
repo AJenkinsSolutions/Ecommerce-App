b7c12a3de97914b539904fc40ad8c726b72e7129
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-16 22:20:28 -0500

    Add: Api service integration

--- src/app/shared/service/product-api.service.ts
+++ src/app/shared/service/product-api.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from '../../models/product.interface';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductApiService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  baseUrl: string = 'https://fakestoreapi.com/';
+  productsEndpoint: string = 'products';
+
+  products$: Observable<IProduct[]>;
+
+
+  constructor() {
+
+    this.products$ = this.getProducts();
+
+    console.log("Info: Products array" + this.products$.forEach(obj => console.log(obj)) );
+    
+   }
+
+  
+  getProducts(){
+
+     //A Dynamic way of adding a property to the api response 
+     return this.http.get<IProduct[]>(this.baseUrl + this.productsEndpoint)
+     .pipe(
+       map((products) =>{
+         return products.map((product) => {
+ 
+           return {...product, quantity: 1}
+ 
+         })
+       })
+     )
+
+  }
+
 }
