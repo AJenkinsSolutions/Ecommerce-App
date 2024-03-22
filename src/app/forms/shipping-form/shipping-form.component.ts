47ce5aa199f85174b993ebbabd5a93a6a57338b2
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-22 11:00:07 -0500

    Add: Shippiong form

---
+++ src/app/forms/shipping-form/shipping-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './shipping-form.component.html',
+  styleUrl: './shipping-form.component.scss'
+})
+export class ShippingFormComponent {
+
+}
