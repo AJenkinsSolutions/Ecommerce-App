40353d4471aaceb6e85f7e1b8aed3cf9d5f9793f
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-22 17:07:40 -0500

    Add: order component

---
+++ src/app/pages/order/order.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './order.component.html',
+  styleUrl: './order.component.scss'
+})
+export class OrderComponent {
+
+  //This will hold the order summary information
+  //Which will take 
+
+}
