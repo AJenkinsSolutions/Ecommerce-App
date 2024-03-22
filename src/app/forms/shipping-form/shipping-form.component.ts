e7b5d343666311511f1f72685fd6ba92c31251e3
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-22 17:08:10 -0500

    Add: shippiong form impl

--- src/app/forms/shipping-form/shipping-form.component.ts
+++ src/app/forms/shipping-form/shipping-form.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shipping-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './shipping-form.component.html',
   styleUrl: './shipping-form.component.scss'
 })
-export class ShippingFormComponent {
+export class ShippingFormComponent implements OnInit{
+
+
+  shippingForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder){}
+
+  
+  
+  
+  ngOnInit(): void {
+    
+    //Create Validators
+    this.shippingForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern(("^\\([0-9]{3}\\) {1}[0-9]{3}-{1}[\\d]{4}$"))]],
+      city: ['', Validators.required]
+    })
+
+
+
+  }
+
+
+  onSubmit(){
+    console.log("info: Shipping form submit hit")
+    if(this.shippingForm.valid){
+      console.log("form valid")
+
+      const completedForm =  this.shippingForm.getRawValue();
+      console.log("form"+ completedForm)
+    }
+  }
+
+
+
+
 
 }
--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -3,11 +3,12 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatSelectModule} from '@angular/material/select'
 import { MatButtonModule } from '@angular/material/button';
+import { ShippingFormComponent } from '../../forms/shipping-form/shipping-form.component';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+  imports: [ShippingFormComponent ,MatCardModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
