3693d3410538ca9f3e370613c2970a66487f5aa5
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-22 10:59:59 -0500

    Add: basic html for checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
+
+
+
+  onSubmit(){
+
+  }
 
 }
