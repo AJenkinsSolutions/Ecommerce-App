013a01bde5161b65fdef1236a8a80b49a7dbf8b9
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-23 12:20:30 -0500

    Add: signal store to order form, and order sum

--- src/app/forms/order-form/order-form.component.ts
+++ src/app/forms/order-form/order-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectionStrategy} from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppState } from '../../ngrx/state/app.state';
 
@@ -11,17 +11,20 @@ import { signalStore, withState } from '@ngrx/signals';
 import { FormSignal, UserStore } from '../../signals/form.signal';
 import { IShippingInform } from '../../models/order-summary-interface';
 
+
 @Component({
   selector: 'app-order-form',
   standalone: true,
   imports: [ReactiveFormsModule, FormsModule, CommonModule,],
-  providers: [UserStore],
   templateUrl: './order-form.component.html',
-  styleUrl: './order-form.component.scss'
+  styleUrl: './order-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+  
 })
 export class OrderFormComponent implements OnInit{
 
-  // userStore = inject(UserStore);
+  
+  userStore = inject(UserStore);
 
   orderFormGroup: FormGroup = new FormGroup({});
 
@@ -60,6 +63,9 @@ export class OrderFormComponent implements OnInit{
 
       const completedForm: IShippingInform =  this.orderFormGroup.value;
       console.log(completedForm)
+
+      this.userStore.updateUser('hannah', 9, completedForm)
+      
       
       this.navigateToOrderSummary()
 
--- src/app/pages/order-summary/order-summary.component.ts
+++ src/app/pages/order-summary/order-summary.component.ts
@@ -1,30 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Signal, inject } from '@angular/core';
 import { AppState } from '../../ngrx/state/app.state';
 import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
 import * as CartSelector from '../../ngrx/selectors/cart.selector';
 import { Observable } from 'rxjs';
 import { IProduct } from '../../models/product.interface';
-
+import { signalStore, withState } from '@ngrx/signals';
+import { FormSignal, UserStore } from '../../signals/form.signal';
 @Component({
   selector: 'app-order-summary',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './order-summary.component.html',
-  styleUrl: './order-summary.component.scss'
+  styleUrl: './order-summary.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrderSummaryComponent implements OnInit{
 
   //Once users has submitted order form we need to navigate to the order summary page to confirm order
 
+
+userStore = inject(UserStore);
+
   cartItems$: Observable<IProduct[]> = this.store.select(CartSelector.seletctCartProducts);
   totalPrice$: Observable<number> =  this.store.select(CartSelector.selectTotalPrice);
 
-  constructor(private store: Store<AppState>){
+  constructor(private store: Store<AppState>, ){
 
-    console.log(this.cartItems$.forEach((obj) => console.log(obj)));
-    console.log(this.totalPrice$);
-  }
+  //   console.log(this.cartItems$.forEach((obj) => console.log(obj)));
+  //   console.log(this.totalPrice$);
+
+    console.log(this.userStore.name());
+    console.log(this.userStore.age());
+  
+}
 
   ngOnInit(): void {
 
