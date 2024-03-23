fd47355118e09cd4e8c401e9843813bd83b580fa
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-23 11:42:05 -0500

    Add: cart store to order summary

--- src/app/pages/order-summary/order-summary.component.ts
+++ src/app/pages/order-summary/order-summary.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from '../../ngrx/state/app.state';
 import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
+import * as CartSelector from '../../ngrx/selectors/cart.selector';
+import { Observable } from 'rxjs';
+import { IProduct } from '../../models/product.interface';
 
 @Component({
   selector: 'app-order-summary',
@@ -12,9 +15,16 @@ import { CommonModule } from '@angular/common';
 })
 export class OrderSummaryComponent implements OnInit{
 
+  //Once users has submitted order form we need to navigate to the order summary page to confirm order
 
+  cartItems$: Observable<IProduct[]> = this.store.select(CartSelector.seletctCartProducts);
+  totalPrice$: Observable<number> =  this.store.select(CartSelector.selectTotalPrice);
 
-  constructor(private store: Store<AppState>){}
+  constructor(private store: Store<AppState>){
+
+    console.log(this.cartItems$.forEach((obj) => console.log(obj)));
+    console.log(this.totalPrice$);
+  }
 
   ngOnInit(): void {
 
